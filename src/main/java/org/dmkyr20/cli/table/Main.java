03476686ea5f0af24b6c4a98424b150bccdc52df
Dmytro <dmkyr20@gmail.com>
2020-06-02 15:33:26 +0300

    (WIP) Added plugin for jar building and CommandLineOprions

--- src/main/java/org/dmkyr20/cli/table/Main.java
+++ src/main/java/org/dmkyr20/cli/table/Main.java
@@ -1,9 +1,36 @@
 package org.dmkyr20.cli.table;
 
-// TODO add Unicode table char multiplier
+import org.apache.commons.cli.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(5);
-        System.out.println(sb.toString() + "<-");
+        Options commandLineOptions = new Options();
+        Option input = new Option("i", "input", true, "input file path");
+        Option output = new Option ("o", "output", true, "output file");
+        Option print = new Option ("p", "print", false, "print output to console");
+        input.setRequired(true);
+        commandLineOptions.addOption(input);
+        commandLineOptions.addOption(output);
+        commandLineOptions.addOption(print);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(commandLineOptions, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", commandLineOptions);
+
+            System.exit(1);
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        System.out.println(inputFilePath);
+        System.out.println(outputFilePath);
     }
 }
